112a50cdaa1ce832303a1adfd239a91c5b49aa13
KK09020 <kiku09020@gmail.com>
2024-07-28 18:09:20 +0900

    スコアに応じて、蜂の巣生成確率上昇するように変更

--- Assets/Scripts/Game/Tree/Branch/HiveGenerator.cs
+++ Assets/Scripts/Game/Tree/Branch/HiveGenerator.cs
@@ -1,3 +1,4 @@
+using Game.Score;
 using UnityEngine;
 
 namespace Game.Tree.Branch.Item
@@ -5,15 +6,23 @@ namespace Game.Tree.Branch.Item
 	/// <summary> 蜂の巣生成クラス </summary>
 	public class HiveGenerator : BranchItemGenerator
 	{
-		[SerializeField] float generatedProb = 0.5f;
+		[SerializeField] float generatedProb = 0.1f;
+		[SerializeField] float generatedProbMax = 0.5f;
+		[SerializeField] ScoreController scoreController;
 
-		public override float GeneratedProb => generatedProb;
+		public override float GeneratedProb => GetGeneratedProb();
 
 		//------------------------------------------------------------
 
 		public override BranchItem Generate()
 		{
 			return pool.GetPooledObject(Vector3.zero);
+		}
+
+		/// <summary> スコアに応じて生成確率変える </summary>
+		float GetGeneratedProb()
+		{
+			return Mathf.Min(generatedProb * scoreController.ScoreLevelRate, generatedProbMax);
 		}
 	}
 }
